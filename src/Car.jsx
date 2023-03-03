8babf290433fd5687d46f9a7bcf353928a8790d0
DanielVarjaskéri <varjaskerid@gmail.com>
2023-03-03 23:01:21 +0100

    cleanUpAgain

--- src/Car.jsx
+++ src/Car.jsx
@@ -6,8 +6,7 @@ import { BufferGeometry } from 'three';
 import gsap from "gsap";
 
 
-// based on "Chevrolet Corvette (C7)" (https://sketchfab.com/3d-models/chevrolet-corvette-c7-2b509d1bce104224b147c81757f6f43a) 
-// by Martin Trafas (https://sketchfab.com/Bexxie) licensed under CC-BY-4.0 (http://creativecommons.org/licenses/by/4.0/)
+
 export function Car(props) {
 
   
@@ -34,36 +33,18 @@ export function Car(props) {
 
 
 
-    
-
-//   useFrame((state, delta) => {
-//     let t = state.clock.getElapsedTime();
-
-//     let group = gltf.scene.children[0].children[0].children[0];
-//     group.children[0].rotation.x = t * 2;
-//     group.children[2].rotation.x = t * 2;
-//     group.children[4].rotation.x = t * 2;
-//     group.children[6].rotation.x = t * 2;
-//   });
-
 
 
   function openTrunk(open){
-   // gltf.scene.children[0].children[76].children[0].rotation.x = 0;
-
     if (open){ 
     gsap.to(gltf.scene.children[0].children[76].children[0].rotation,{
       x: 1,
-      
       duration: 2,
-    
     });
   } else{
     gsap.to(gltf.scene.children[0].children[76].children[0].rotation,{
       x: 0,
-      
       duration: 1,
-    
     });
   }
   }
@@ -81,13 +62,6 @@ export function Car(props) {
     gltf.scene.children[2].rotation.x = t * 3.5;
     gltf.scene.children[4].rotation.x = t * 3.5;
     gltf.scene.children[5].rotation.x = t * 3.5;
-    
-    // let group = gltf.scene.children[0].children[0].children[0];
-    // group.children[0].rotation.x = t * 2;
-    // group.children[2].rotation.x = t * 2;
-    // group.children[4].rotation.x = t * 2;
-    // group.children[6].rotation.x = t * 2;
-    //let t = -state.clock.getElapsedTime() * 0.68;
   });
 
   
@@ -95,7 +69,6 @@ export function Car(props) {
   
 
   return (
-    
         <primitive object={gltf.scene} />
   )
 }
